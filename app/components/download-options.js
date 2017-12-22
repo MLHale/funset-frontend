96e3d68c1265d51e99d490a52c83152defc154ca
Matt Hale <mlhale@unomaha.edu>
2017-12-22 16:02:16 -0600

    fixed svg zoom layer handling

--- app/components/download-options.js
+++ app/components/download-options.js
@@ -7,14 +7,19 @@ export default Component.extend(FileSaverMixin,{
     downloadSvg(){
 
       var svg = d3.select("svg")
-        .attr("title", "test2")
+        .attr("title", config.host+"-svg")
         .attr("version", 1.1)
         .attr("xmlns", "http://www.w3.org/2000/svg");
 
-      svg.selectAll(".zoom-layer").remove()
       var html = svg.node().parentNode.innerHTML;
 
-      this.saveFileAs(config.host+"-svg-download.svg", html, "image/svg+xml");
+      var domhtml = Ember.$.parseHTML(html);
+
+      //remove zoom layer so it doesn't block the graph
+      $('.zoom-layer',domhtml).remove();
+
+      //save to file
+      this.saveFileAs(config.host+"-svg-download.svg", domhtml[1].outerHTML, "image/svg+xml");
     },
     downloadJSON(){
       this.saveFileAs(config.host+"-json-download.json", JSON.stringify(this.get('navigation.clusterjson.content')), "application/json");
