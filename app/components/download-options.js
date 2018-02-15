c2d4ce55467997d4ce2b9287211016c76dce7295
Matt Hale <mlhale@unomaha.edu>
2018-02-15 00:25:46 -0600

    added license headers

--- app/components/download-options.js
+++ app/components/download-options.js
@@ -1,3 +1,16 @@
+/**
+ * @Author: Matthew Hale <mlhale>
+ * @Date:   2018-02-14T23:03:42-06:00
+ * @Email:  mlhale@unomaha.edu
+ * @Filename: download-options.js
+ * @Last modified by:   mlhale
+ * @Last modified time: 2018-02-15T00:22:40-06:00
+ * @License: Funset is a web-based BIOI tool for visualizing genetic pathway information. This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.
+ * @Copyright: Copyright (C) 2017 Matthew L. Hale, Dario Ghersi, Ishwor Thapa
+ */
+
+
+
 import Component from '@ember/component';
 import d3 from 'npm:d3'
 import config from '../config/environment';
