5d4bf7f7cfccec7af2275ad807121d5096dd2e4b
Matt Hale <mlhale@unomaha.edu>
2018-02-24 01:35:27 -0600

    removed some test settings

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,8 +3,8 @@
  * @Date:   2018-02-14T23:03:42-06:00
  * @Email:  mlhale@unomaha.edu
  * @Filename: index.js
- * @Last modified by:   mlhale
- * @Last modified time: 2018-02-16T01:59:12-06:00
+ * @Last modified by:   matthale
+ * @Last modified time: 2018-02-24T01:26:25-06:00
  * @License: Funset is a web-based BIOI tool for visualizing genetic pathway information. This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.
  * @Copyright: Copyright (C) 2017 Matthew L. Hale, Dario Ghersi, Ishwor Thapa
  */
@@ -35,7 +35,7 @@ export default Controller.extend({
   background: '',
   namespaceselected: {code: 'bp', name: 'Biological Process'},
   namespaceOptions: [{code: 'bp', name: 'Biological Process'},{code: 'mf', name: 'Molecular Function'},{code: 'cc', name: 'Cellular Component'}],
-  genelist: 'HLA-DMA,HLA-DMB',
+  genelist: '',
   genelistValidation: [{
     message: 'Genes must be listed in a comma seperated value format with no white spaces or line feed characters. e.g. gene1,gene2,...',
     validate: (inputValue) => {
