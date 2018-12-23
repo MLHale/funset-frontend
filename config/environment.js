b216bdfdf5142fd43430a0a20be412688bc4406d
mlhale <mlhale@unomaha.edu>
2018-12-23 04:20:11 -0600

    dependency updates, security patches, and UI revisions

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@
  * @Email:  mlhale@unomaha.edu
  * @Filename: environment.js
  * @Last modified by:   matthale
- * @Last modified time: 2018-03-08T14:18:17-06:00
+ * @Last modified time: 2018-12-23T02:43:23-06:00
  * @License: Funset is a web-based BIOI tool for visualizing genetic pathway information. This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.
  * @Copyright: Copyright (C) 2017 Matthew L. Hale, Dario Ghersi, Ishwor Thapa
  */
@@ -21,7 +21,7 @@ module.exports = function(environment) {
     rootURL: '/',
     bURL: '/',
     locationType: 'hash',
-    host: 'http://funset.unomaha.edu',
+    host: 'http://funset.uno',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
@@ -60,7 +60,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     // here you can enable a production-specific feature
-    ENV.host = 'http://funset.unomaha.edu';
+    ENV.host = 'http://funset.uno';
     ENV.locationType = 'hash';
     ENV.rootURL = "static/ember/"
   }
